8ab575f86077b46561f4b6e5da758d465fdcc954
philpatt <phillip.s.patterson1@gmail.com>
2018-04-05 11:23:27 -0700

    added console logs for better understanding

--- 01_introduction_to_algorithms/javascript/01_binary_search.js
+++ 01_introduction_to_algorithms/javascript/01_binary_search.js
@@ -3,17 +3,25 @@
 function binary_search(list, item) {
   let low = 0;
   let high = list.length - 1;
-  
+  console.log('item is', item)
   while (low <= high) {
-    let mid = Math.floor((low + high) / 2);
-    let guess = list[mid];
+    console.log('low',low,'high',high);
+    let mid = Math.floor((low + high) / 2); //mid index stars at [ 2 ] for this example
+    console.log('mid is low + high / 2 =',mid);
+    
+    let guess = list[mid]; // since mid starts at 5, guess is 5
+    console.log('new guess is list['+mid+'] = ',guess)
     if (guess === item) {
-      return mid;
+      console.log('guess matches item',guess,'===',item);
+      return 'exit';
     }
     if (guess > item) {
       high = mid - 1;
+      console.log('guess of',guess, '> the item', item,'change high to mid - 1, high =',high);      
     } else {
       low = mid + 1;
+      console.log('guess of', guess, '< the item', item,'change low to mid + 1, low =',low);            
+      
     }
   }
   
@@ -23,4 +31,4 @@ function binary_search(list, item) {
 const my_list = [1, 3, 5, 7, 9];
 
 console.log(binary_search(my_list, 3)); // 1
-console.log(binary_search(my_list, -1)); // null
+// console.log(binary_search(my_list, -1)); // null
